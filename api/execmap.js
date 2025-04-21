035c018dd261e9436d5a198ade61b644b31c9800
xssnhub <xssn994@gmail.com>
2025-04-21 17:32:17 +0800

    Add files via upload

--- api/execmap.js
+++ api/execmap.js
@@ -2,12 +2,10 @@ import execMap from '../execmap.json';
 
 export default function handler(req, res) {
   const { code } = req.query;
-
   if (code) {
     const item = execMap[code.toUpperCase()];
     if (item) return res.status(200).json(item);
     return res.status(404).json({ error: "條文代號不存在" });
   }
-
   return res.status(200).json(execMap);
 }
